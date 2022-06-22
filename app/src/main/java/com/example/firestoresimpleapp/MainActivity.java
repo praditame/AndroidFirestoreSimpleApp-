3b81e25771e0415b00267901578aa4810f1e4726
pradita <praditame@gmail.com>
2022-06-22 15:27:45 +0700

    add input activity & layout

--- app/src/main/java/com/example/firestoresimpleapp/MainActivity.java
+++ app/src/main/java/com/example/firestoresimpleapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.firestoresimpleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rv = findViewById(R.id.rv);
+        fab = findViewById(R.id.floating_action_button);
     }
 }
